12c8d2423b3b072c27ef439cedc20ffdb8b7ad88
brunopaulon2021 <you@example.com>
2022-10-16 15:16:28 -0300

    modified 3 files in .

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 /* window.alert('olaaaa'); */
+const menu = document.querySelector('.menuModal')
+const btn = document.querySelector('#btn-modal')
+
+
+function openMenu () {
+
+    if (menu.style.display == 'none')
+       (menu.style.display = 'block')
+
+    else (menu.style.display = 'none')
+       
+}
+
+btn.addEventListener ("click" , openMenu)
