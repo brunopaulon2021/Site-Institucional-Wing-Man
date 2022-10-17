32516f30c3eec5d9adf4e8bee0ebb61cc0a238bb
brunopaulon2021 <paulonbruno9@gmail.com>
2022-10-17 17:00:17 -0300

    modified 3 files in .

--- script.js
+++ script.js
@@ -8,8 +8,11 @@ const openMenu = () => {
     if (menu.style.display == 'none')
         (menu.style.display = 'block')
         (btnOpen.style.scale = '0')
+        (btnClose.style.scale = '3')
         else  
         (menu.style.display = 'none')
+        (btnOpen.style.scale = '3')
+        (btnClose.style.scale = '0')
 
 };
 
