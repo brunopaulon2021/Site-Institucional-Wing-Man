1b82a3c0dffe7c4f5588d2b277e7e4bb87d0b794
brunopaulon2021 <paulonbruno9@gmail.com>
2022-10-18 16:53:58 -0300

    modified script.js and style.css in .

--- script.js
+++ script.js
@@ -16,5 +16,22 @@ const openMenu = () =>
             else (menu.style.display = 'none')
 }
 
+const closeMenu = () => 
+{
+            if (btnClose.style.scale = '3')    
+            
+            {
+                btnClose.style.scale = '0'
+                btnOpen.style.scale = '3'
+            }
+            if  (menu.style.display = 'flex')
+
+            {
+                    menu.style.display = 'none'
+            }
+
+}
+
+
 btnOpen.addEventListener("click", openMenu)
 btnClose.addEventListener("click", closeMenu)
