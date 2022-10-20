a7017bc29b4e6e320ba7678a6d60d578e4081380
brunopaulon2021 <paulonbruno9@gmail.com>
2022-10-19 21:13:08 -0300

    update contato.html, index.html and style.css

---
+++ contato.js
@@ -0,0 +1,38 @@
+/* window.alert('olaaaa'); */
+const menu = document.querySelector('.menuModal')
+const btnOpen = document.querySelector('#btn-open')
+const btnClose = document.querySelector('#btn-close')
+
+const openMenu = () => 
+{
+            if (menu.style.display == 'none')
+                
+            {
+                menu.style.display = 'block'
+                
+                btnOpen.style.scale = '0'
+                btnClose.style.scale = '3'
+            }
+
+            else (menu.style.display = 'none')
+}
+
+const closeMenu = () => 
+{
+            if (btnClose.style.scale = '3')    
+            
+            {
+                btnClose.style.scale = '0'
+                btnOpen.style.scale = '3'
+            }
+            if  (menu.style.display = 'block')
+
+            {
+                    menu.style.display = 'none'
+            }
+
+}
+
+
+btnOpen.addEventListener("click", openMenu)
+btnClose.addEventListener("click", closeMenu)
